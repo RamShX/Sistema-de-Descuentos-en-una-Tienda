e5151f9072d76c3455cb6923cf3e774853eb4416
Ramshley P. <ramshleyv@gmail.com>
2025-03-25 18:23:58 -0400

    Se agregó el context

---
+++ SistemaDescuento.Domain/Context/Producto.cs
@@ -0,0 +1,19 @@
+﻿
+using SistemaDescuento.Domain.Interfeces;
+
+namespace SistemaDescuento.Domain.Context
+{
+    public class Producto
+    {
+        private readonly IDescuento _descuento;
+        public Producto(IDescuento descuento)
+        {
+            _descuento = descuento;
+        }
+
+        public double AplicarDescuento(double precio)
+        {
+            return _descuento.CalcularDescuento(precio);
+        }
+    }
+}
